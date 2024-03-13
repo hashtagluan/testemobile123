baf7b230032a7d4792bc14e522d318ce02cdc787
hashtagluan <128554958+hashtagluan@users.noreply.github.com>
2024-03-13 20:49:27 -0300

    Add files via upload

---
+++ scr/pages/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomePage = () => {
+
+  return(
+
+    <View> 
+    
+    <Text> Puc Minas </Text>
+    
+    
+    </View>
+
+  );
+
+}
+
+export default HomePage;
