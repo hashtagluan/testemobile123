a9c1638b5af4371061f202fb2466dc6ea74437ff
hashtagluan <128554958+hashtagluan@users.noreply.github.com>
2024-03-13 20:45:52 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import HomePage from './scr/pages/HomePage'
+
+const App = () => {
+
+  return(
+
+    <HomePage />
+
+  );
+
+}
+
+export default App;
